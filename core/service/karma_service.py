58cdc520b0e4ebeb596d260dfd440acadcc227b3
arkencl <fortinbra@protonmail.com>
2020-04-10 16:53:56 +0200

    implement leaderboard functionality

--- core/service/karma_service.py
+++ core/service/karma_service.py
@@ -7,21 +7,21 @@ from core.model.karma_member import KarmaMember
 class KarmaService:
 
     def __init__(self):
-        self._db = Database('mongo', 27017, 'aura').db
+        self._karma = Database('mongo', 27017, 'aura').db.karma
         self._filter_query = dict(guild_id="", member_id="", karma_type="")
         self._increase_karma = {"$inc": {'karma': int(1)}}
 
     def upsert_karma_member(self, member: KarmaMember):
-        karma = self._db.karma
         self._filter_query['guild_id'] = member.guild_id
         self._filter_query['member_id'] = member.member_id
         self._filter_query['karma_type'] = member.karma_type
-        karma.update_one(filter=self._filter_query, update=self._increase_karma,
-                         upsert=True)
+        self._karma.update_one(filter=self._filter_query, update=self._increase_karma,
+                               upsert=True)
 
-    def get_top_karma_members(self, guild_id: int, limit: int, karma_type: str):
+    def get_top_karma_members(self, guild_id: str, limit: int, karma_type: str):
         filter_guild = dict(guild_id=guild_id, karma_type=karma_type)
-        return self._db.karma.find(filter_guild).sort([('karma', pymongo.ASCENDING)]).limit(limit)
+        print(filter_guild)
+        return self._karma.find(filter_guild).sort([('karma', pymongo.ASCENDING)]).limit(limit)
 
     def delete_karma_member(self, member: KarmaMember):
         print()
@@ -33,7 +33,7 @@ class KarmaService:
         self._filter_query['guild_id'] = member.guild_id
         self._filter_query['member_id'] = member.member_id
         self._filter_query['karma_type'] = member.karma_type
-        document = self._db.karma.find_one(filter=self._filter_query)
+        document = self._karma.find_one(filter=self._filter_query)
         if document is None:
             return 0
         else:
