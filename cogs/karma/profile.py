502a9943f08470b06d379dc81814a33b447495fd
arkencl <fortinbra@protonmail.com>
2020-05-07 15:52:30 +0200

    rework config.py to be module level refactoring modify VERSION_HISTORY.MD

--- cogs/karma/profile.py
+++ cogs/karma/profile.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 from core.model.member import KarmaMember
 from core.service.karma_service import KarmaService
-from util.config import ConfigStore
+from util.config import config
 
 
 # Karma Profile Class, users other than moderators and admins can only see their own karma or profile.
@@ -13,18 +13,15 @@ class KarmaProfile(commands.Cog):
         self._bot = bot
         bot.remove_command("help")
         self._karma_service = KarmaService()
-        self._configManager = ConfigStore()
-        self._config = self._configManager.config
-        self._roles = self._configManager.roles
 
     # get karma of yourself without any arguments, get karma of others if admin or moderator with user_id.
     @commands.command()
     async def karma(self, ctx):
-        guild_id: str = self._config['guild']
+        guild_id: str = config['guild']
         guild = self._bot.get_guild(int(guild_id))
         roles = ctx.message.author.roles
-        if len(ctx.message.mentions) == 1 and (self._roles['admin'] in [role.name for role in roles]
-                                               or self._roles['moderator'] in [role.name for role in roles]):
+        if len(ctx.message.mentions) == 1 and (roles()['admin'] in [role.name for role in roles]
+                                               or roles()['moderator'] in [role.name for role in roles]):
             message = ctx.message
             member = message.mentions[0]
             if not self._bot.get_user(self._bot.user.id).mentioned_in(message) and guild.get_member(
@@ -34,7 +31,7 @@ class KarmaProfile(commands.Cog):
                 await ctx.channel.send('{} has earned a total of {} karma'
                                        .format(member.name + '#' + member.discriminator, karma))
         elif len(ctx.message.mentions) == 0:
-            karma_member = KarmaMember(self._config['guild'], ctx.message.author.id)
+            karma_member = KarmaMember(config['guild'], ctx.message.author.id)
             karma = self._karma_service.aggregate_member_karma(karma_member)
             await ctx.channel.send('{} has earned a total of {} karma'
                                    .format(ctx.message.author.name + '#' + ctx.author.discriminator, karma))
---
+++ cogs/karma/reduce.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+from discord.ext.commands import has_any_role
+
+from core.model.member import KarmaMember
+from core.service.karma_service import KarmaService
+
+# Karma Reducer Class remove karma
+from util.config import config
+
+
+class KarmaReducer(commands.Cog):
+
+    def __init__(self, bot):
+        self._bot = bot
+        self._karma_service = KarmaService()
+
+    # remove all karma from member
+    @commands.command()
+    async def reset(self, ctx, member):
+        guild_id: str = config['guild']
+        self._karma_service.delete_all_karma(KarmaMember(guild_id, member))
+        await ctx.channel.send('Removed all Karma from {}'.format(member))
+
+
+class KarmaBlocker(commands.Cog):
+
+    def __init__(self, bot):
+        self._bot = bot
+        self._karma_service = KarmaService()
+
+    async def blacklist(self, ctx, member):
+        guild_id: str = config['guild']
+        await ctx.channel.send('Blacklisted {}'.format(member))
+
+    async def whitelist(self, ctx, member):
+        guild_id: str = config['guild']
+        await ctx.channel.send('Whitelisted {}'.format(member))
