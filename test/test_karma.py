7823d5e014a4c4025242df2bb3096f1badfe2cb2
arkencl <fortinbra@protonmail.com>
2020-05-10 16:09:18 +0200

    modify producer.py impl a single test_karma.py case

--- test/test_karma.py
+++ test/test_karma.py
@@ -1,16 +1,28 @@
 import unittest
 
+import mongomock
+
+from cogs.karma.producer import KarmaProducer
+from core.model.member import KarmaMember
+from core.service.karma_service import KarmaService
+
 if __name__ == '__main__':
     unittest.main()
 
 
 # Verify that karma is given through all the possible permutations
-class KarmaGiven(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class KarmaChange(unittest.TestCase):
+    def test_karma_increases(self):
+        karma_storage = mongomock.MongoClient().db.karma
+        karma_service = KarmaService(karma_storage)
+        karma_member = KarmaMember('1', '1', '1', '1')
+        karma_service.upsert_karma_member(karma_member, True)
+        assert karma_service.aggregate_member_by_karma(karma_member) == 1
+        karma_service.upsert_karma_member(karma_member, True)
+        assert karma_service.aggregate_member_by_karma(karma_member) == 2
 
 
 # Verify that karma is not given because of running cooldown
 class KarmaCooldown(unittest.TestCase):
-    def test_something(self):
+    def test_karma_not_given(self):
         self.assertEqual(True, False)
