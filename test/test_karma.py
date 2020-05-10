99ad039e615776239b6cc587f1f1c987bc1720a1
arkencl <fortinbra@protonmail.com>
2020-05-10 16:31:52 +0200

    change VERSION_HISTORY.MD reformat in datasource.py refactor in reduce.py add test case and modify old one in test_karma.py

--- test/test_karma.py
+++ test/test_karma.py
@@ -2,7 +2,6 @@ import unittest
 
 import mongomock
 
-from cogs.karma.producer import KarmaProducer
 from core.model.member import KarmaMember
 from core.service.karma_service import KarmaService
 
@@ -10,7 +9,7 @@ if __name__ == '__main__':
     unittest.main()
 
 
-# Verify that karma is given through all the possible permutations
+# Verify that karma_services methods are working properly on a mocked mongodb
 class KarmaChange(unittest.TestCase):
     def test_karma_increases(self):
         karma_storage = mongomock.MongoClient().db.karma
@@ -20,6 +19,18 @@ class KarmaChange(unittest.TestCase):
         assert karma_service.aggregate_member_by_karma(karma_member) == 1
         karma_service.upsert_karma_member(karma_member, True)
         assert karma_service.aggregate_member_by_karma(karma_member) == 2
+        for doc in karma_service.aggregate_member_by_channels(karma_member):
+            assert doc['karma'] == 2
+
+    def test_karma_resets(self):
+        karma_storage = mongomock.MongoClient().db.karma
+        karma_service = KarmaService(karma_storage)
+        karma_member = KarmaMember('1', '1', '1', '1')
+        karma_service.upsert_karma_member(karma_member, True)
+        assert karma_service.aggregate_member_by_karma(karma_member) == 1
+        karma_service.delete_all_karma(karma_member.guild_id,
+                                       karma_member.member_id)
+        assert karma_service.aggregate_member_by_karma(karma_member) is None
 
 
 # Verify that karma is not given because of running cooldown
