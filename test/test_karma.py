1203ebe2a066d4e8b9b7599b302455eafbc2b561
arkencl <fortinbra@protonmail.com>
2020-05-10 19:20:17 +0200

    modify tests

--- test/test_karma.py
+++ test/test_karma.py
@@ -11,29 +11,27 @@ if __name__ == '__main__':
 
 # Verify that karma_services methods are working properly on a mocked mongodb
 class KarmaChange(unittest.TestCase):
+
+    karma_storage = mongomock.MongoClient().db.karma
+    karma_service = KarmaService(karma_storage)
+    karma_member = KarmaMember('1', '1', '1', '1')
+
     def test_karma_increases(self):
-        karma_storage = mongomock.MongoClient().db.karma
-        karma_service = KarmaService(karma_storage)
-        karma_member = KarmaMember('1', '1', '1', '1')
-        karma_service.upsert_karma_member(karma_member, True)
-        assert karma_service.aggregate_member_by_karma(karma_member) == 1
-        karma_service.upsert_karma_member(karma_member, True)
-        assert karma_service.aggregate_member_by_karma(karma_member) == 2
-        for doc in karma_service.aggregate_member_by_channels(karma_member):
+        self.karma_service.upsert_karma_member(self.karma_member, True)
+        assert self.karma_service.aggregate_member_by_karma(self.karma_member) == 1
+        self.karma_service.upsert_karma_member(self.karma_member, True)
+        assert self.karma_service.aggregate_member_by_karma(self.karma_member) == 2
+        for doc in self.karma_service.aggregate_member_by_channels(self.karma_member):
             assert doc['karma'] == 2
 
     def test_karma_resets(self):
-        karma_storage = mongomock.MongoClient().db.karma
-        karma_service = KarmaService(karma_storage)
-        karma_member = KarmaMember('1', '1', '1', '1')
-        karma_service.upsert_karma_member(karma_member, True)
-        assert karma_service.aggregate_member_by_karma(karma_member) == 1
-        karma_service.delete_all_karma(karma_member.guild_id,
-                                       karma_member.member_id)
-        assert karma_service.aggregate_member_by_karma(karma_member) is None
+        self.karma_service.delete_all_karma(self.karma_member.guild_id,
+                                            self.karma_member.member_id)
+        assert self.karma_service.aggregate_member_by_karma(self.karma_member) is None
 
 
-# Verify that karma is not given because of running cooldown
-class KarmaCooldown(unittest.TestCase):
-    def test_karma_not_given(self):
-        self.assertEqual(True, False)
+# Verify that karma giving works on all possible permutations
+class KarmaGiving(unittest.TestCase):
+
+    def test_karma_resets(self):
+        assert 0 == 1
