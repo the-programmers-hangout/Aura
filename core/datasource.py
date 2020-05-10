31958c0268db74bcef87db0aae98a6cb7527dc1b
Princess Lana <ianagbip1oti@gmail.com>
2020-05-09 19:08:00 -0700

    Remove unneeded property

--- core/datasource.py
+++ core/datasource.py
@@ -7,8 +7,4 @@ class DataSource(object):
     def __init__(self, host, port, username, password, name):
         self._client = MongoClient(host=host, port=port, username=username, password=password,
                                    authMechanism='SCRAM-SHA-256')
-        self._db = self._client.get_database(name)
-
-    @property
-    def db(self):
-        return self._db
+        self.db = self._client.get_database(name)
