82d865d94c9dc39234464b59baf2ae4234977588
arkencl <fortinbra@protonmail.com>
2020-05-10 19:20:17 +0200

    modify tests

--- test/test_blacklist.py
+++ test/test_blacklist.py
@@ -11,23 +11,27 @@ if __name__ == '__main__':
 
 # Verify that member is blacklisted from giving
 class MemberIsBlackListed(unittest.TestCase):
+
+    blacklisted = mongomock.MongoClient().db.blacklist
+    blocker_service = BlockerService(blacklisted)
+    member = Member('1', '1')
+    member_2 = Member('1', '2')
+    blocker_service.blacklist(member)
+
     def test_blacklisted_correctly(self):
-        blacklisted = mongomock.MongoClient().db.blacklist
-        blocker_service = BlockerService(blacklisted)
-        member = Member('1', '1')
-        member_2 = Member('1', '2')
-        blocker_service.blacklist(member)
-        assert blocker_service.find_member(member) is not None
-        assert blocker_service.find_member(member_2) is None
+        assert self.blocker_service.find_member(self.member) is not None
+        assert self.blocker_service.find_member(self.member_2) is None
 
 
 # Verify that member is whitelisted after being blacklisted
 class MemberIsWhiteListed(unittest.TestCase):
+
+    blacklisted = mongomock.MongoClient().db.blacklist
+    blocker_service = BlockerService(blacklisted)
+    member = Member('1', '1')
+
     def test_whitelisted_correctly(self):
-        blacklisted = mongomock.MongoClient().db.blacklist
-        blocker_service = BlockerService(blacklisted)
-        member = Member('1', '1')
-        blocker_service.blacklist(member)
-        assert blocker_service.find_member(member) is not None
-        blocker_service.whitelist(member)
-        assert blocker_service.find_member(member) is None
+        self.blocker_service.blacklist(self.member)
+        assert self.blocker_service.find_member(self.member) is not None
+        self.blocker_service.whitelist(self.member)
+        assert self.blocker_service.find_member(self.member) is None
