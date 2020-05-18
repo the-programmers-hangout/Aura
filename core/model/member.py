2e2ae64f726fe5e3ec0061ffc3168f71d343c7fa
arkencl <fortinbra@protonmail.com>
2020-05-18 18:24:39 +0200

    fix double karma bug

--- core/model/member.py
+++ core/model/member.py
@@ -1,7 +1,7 @@
 # simple entity class for karma associated with a member
 class KarmaMember:
     def __init__(self, guild_id: str, member_id: str, channel_id: str = "", message_id: str = "",
-                 karma: int = 1):
+                 karma: int = 0):
         self.guild_id = str(guild_id)
         self.member_id = str(member_id)
         self.channel_id = str(channel_id)
