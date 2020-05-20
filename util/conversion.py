f1a6c671efa51d043eae81799f62c8fab307bdee
arkencl <fortinbra@protonmail.com>
2020-05-20 16:58:42 +0200

    fix logging in main module fix reduce.py both KarmaBlocker and KarmaReducer to work with all calls. fix profile and karma calls edit VERSION_HISTORY.MD

---
+++ util/conversion.py
@@ -0,0 +1,26 @@
+import logging
+import re
+
+import discord
+
+log = logging.getLogger(__name__)
+
+mention_pattern = r'[^@!<>]+'
+mention_regex = re.compile(mention_pattern)
+
+
+# convert member_list with mixed ids and members to a pure list of members
+# the list returned only contains non-bot members
+async def convert_content_to_member_list(ctx, argument_list):
+    guild = ctx.guild
+    pure_list = []
+    logging.info('Conversion to pure list \n original list: {}'.format(argument_list))
+    for content in argument_list:
+        result = re.search(mention_regex, content)
+        if result is not None:
+            log.info(result.group(0))
+            content = result.group(0)
+        member = guild.get_member(int(content))
+        if member is not None and not member.bot:
+            pure_list.append(member)
+    return pure_list
