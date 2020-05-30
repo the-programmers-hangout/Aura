d232cea427f6634d1611129b44476cb1f93f490c
arkencl <fortinbra@protonmail.com>
2020-05-30 22:29:53 +0200

    refactoring of help.py, split embed into two funcs

--- cogs/help.py
+++ cogs/help.py
@@ -61,46 +61,54 @@ class HelpMenu(commands.Cog):
     # build the help embed that is to be returned to the user
     async def build_help_embed(self, ctx, args) -> Embed:
         embed = Embed(colour=Color.dark_gold())
-        # if no args, show help overview (all commands executable by user)
         if len(args) == 0:
-            embed.title = 'Help Menu'
-            embed.description = 'Use {}help <command> for more information'.format(config['prefix'])
-            cog_mapping = self.bot.cogs
-            not_rendered_counter = 0
-            for cog in cog_mapping:
-                command_list = cog_mapping[cog].get_commands()
-                if len(command_list) > 0:
-                    embed_val = ''
-                    for command in command_list:
-                        # check if command is executable for caller
-                        is_executable = False
-                        try:
-                            is_executable = await command.can_run(ctx)
-                        except CommandError:
-                            pass
-                        if is_executable:
-                            # if it is include in the embed, otherwise no need for user to know about the command.
-                            embed_val += command.name + '\n'
-                    if embed_val != '':
-                        embed.add_field(name='**' + cog + '**', value=embed_val, inline=True)
-                else:
-                    not_rendered_counter -= 1
-            # use the not rendered counter to insert zero width space fields to properly align the embed
-            embed = add_filler_fields(embed, cog_mapping, '-', not_rendered_counter)
+            # if no args, show help overview (all commands executable by user)
+            embed = await self.overview_embed(embed, ctx)
         else:
-            # return help for the command
-            command = self.bot.get_command(args[0])
-            is_executable = False
-            try:
-                is_executable = await command.can_run(ctx)
-            except CommandError:
-                pass
-            # check if command exists if it does if command is executable by the user like above
-            if command is not None and is_executable:
-                embed.title = command.name
-                embed.description = command.brief
-                embed.add_field(name='**' + 'Structure' + '**', value=command.usage)
+            embed = await self.command_info_embed(embed, ctx, args)
+        return embed
+
+    async def overview_embed(self, embed, ctx):
+        embed.title = 'Help Menu'
+        embed.description = 'Use {}help <command> for more information'.format(config['prefix'])
+        cog_mapping = self.bot.cogs
+        not_rendered_counter = 0
+        for cog in cog_mapping:
+            command_list = cog_mapping[cog].get_commands()
+            if len(command_list) > 0:
+                embed_val = ''
+                for command in command_list:
+                    # check if command is executable for caller
+                    is_executable = False
+                    try:
+                        is_executable = await command.can_run(ctx)
+                    except CommandError:
+                        pass
+                    if is_executable:
+                        # if it is include in the embed, otherwise no need for user to know about the command.
+                        embed_val += command.name + '\n'
+                if embed_val != '':
+                    embed.add_field(name='**' + cog + '**', value=embed_val, inline=True)
             else:
-                embed.title = 'Error: Command not found'
-                embed.description = 'Command does not exist or you do not have the permissions to view it'
+                not_rendered_counter -= 1
+        # use the not rendered counter to insert zero width space fields to properly align the embed
+        embed = add_filler_fields(embed, cog_mapping, '-', not_rendered_counter)
+        return embed
+
+    async def command_info_embed(self, embed, ctx, args):
+        # return help for the command
+        command = self.bot.get_command(args[0])
+        is_executable = False
+        try:
+            is_executable = await command.can_run(ctx)
+        except CommandError:
+            pass
+        # check if command exists if it does if command is executable by the user like above
+        if command is not None and is_executable:
+            embed.title = command.name
+            embed.description = command.brief
+            embed.add_field(name='**' + 'Structure' + '**', value=command.usage)
+        else:
+            embed.title = 'Error: Command not found'
+            embed.description = 'Command does not exist or you do not have the permissions to view it'
         return embed
