0911dd5eb107419fbe3f4ec868bb68b4e004882e
arkencl <fortinbra@protonmail.com>
2020-06-04 21:23:10 +0200

    move bold_field into constants.py

--- util/constants.py
+++ util/constants.py
@@ -7,3 +7,4 @@ revoke_message = 'If you {}, didn\'t intend to give karma to this person,' + \
                  ' react to the' + karma_delete_emoji + 'of your original thanks message'
 embed_max_columns = 3  # 3 because discord embeds can have three fields in a line
 embed_color = Color.dark_gold()
+bold_field = "**{}**"
