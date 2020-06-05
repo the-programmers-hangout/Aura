5e4d81f093ffb81d458ca0710eb8138d1b0029ac
arkencl <fortinbra@protonmail.com>
2020-06-05 23:34:15 +0200

    create basic leaderboard layout

--- cogs/karma/leaderboard.py
+++ cogs/karma/leaderboard.py
@@ -7,7 +7,7 @@ from discord.ext.commands import guild_only
 from core import datasource
 from core.service.karma_service import KarmaService
 from util.config import config
-from util.constants import embed_color
+from util.constants import embed_color, bold_field
 
 log = logging.getLogger(__name__)
 
@@ -21,13 +21,20 @@ class KarmaLeaderboard(commands.Cog):
     @guild_only()
     @commands.command(brief='get a global karma leaderboard, categorical leaderboard or a channel leaderboard',
                       usage='{}leaderboard\n{}leaderboard channel [...]'.format(config['prefix'], config['prefix']))
-    async def leaderboard(self, ctx, *, params=str):
+    async def leaderboard(self, ctx, channel: str = "", time_span: str = ""):
         embed = discord.Embed(colour=embed_color)
         guild = ctx.message.guild
-        leaderboard = list(self.karma_service.aggregate_top_karma_members(str(guild.id)))
-        if len(leaderboard) > 0:
-            for document in leaderboard:
-                member = guild.get_member(int(document['_id']['member_id']))
-                karma = document['karma']
-                embed.add_field(name=member.name + '#' + member.discriminator, value=karma, inline=False)
-            await ctx.channel.send(embed=embed)
+        if channel == "":
+            if time_span == "":
+                leaderboard = list(self.karma_service.aggregate_top_karma_members(str(guild.id)))
+                limit = config['leaderboard']
+                embed.title = f'Top {limit} most helpful people'
+                if len(leaderboard) > 0:
+                    count: int = 1
+                    for document in leaderboard:
+                        member = guild.get_member(int(document['_id']['member_id']))
+                        karma = document['karma']
+                        embed.add_field(name=f'{count}) ' + bold_field.format(member.name + '#' + member.discriminator),
+                                        value=str(karma) + ' karma', inline=False)
+                        count += 1
+                    await ctx.channel.send(embed=embed)
