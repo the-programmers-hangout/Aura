a64c6d1914b0a0692909056dffcb7417c568e30f
arkencl <fortinbra@protonmail.com>
2020-06-08 22:34:06 +0200

    add command descriptions to module.py commands rewrite some command usage text

--- util/constants.py
+++ util/constants.py
@@ -7,10 +7,10 @@ from util.config import karma_delete_emoji
 zero_width_space = '\u200b'
 revoke_message = 'If you {}, didn\'t intend to give karma to this person,' + \
                  ' react to the' + karma_delete_emoji + 'of your original thanks message'
-leaderboard_usage = '{}leaderboard\n{}leaderboard global\n{}leaderboard <#channel_mention>' \
-                    '\n{}leaderboard global days \n'\
+leaderboard_usage = '{}leaderboard\n{}leaderboard <#channel_mention>' \
+                    '\n{}leaderboard (global) (days) \n'\
                     '{}leaderboard ' \
-                    '<#channel_mention> days'
+                    '<#channel_mention> (days)'
 embed_max_columns = 3  # 3 because discord embeds can have three fields in a line
 embed_color = Color.dark_gold()
 bold_field = "**{}**"
