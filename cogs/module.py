0faa9cc06e13fcaeac9a7d15430963768c22dc76
arkencl <fortinbra@protonmail.com>
2020-06-09 11:00:19 +0200

    adding tutor to bot.py as a module help command in module.py can only be reloaded adding docs string to module.py

--- cogs/module.py
+++ cogs/module.py
@@ -18,29 +18,53 @@ class ModuleManager(commands.Cog):
     @has_role(roles()['admin'])
     @commands.command(brief='load a module, module names are listed in the help menu overview',
                       usage='{}load'.format(config['prefix']))
-    async def load(self, ctx, *, module: str):
-        try:
-            self.bot.add_cog(cog_mapping[module])
-        except Exception as e:
-            log.error('{}: {}'.format(type(e).__name__, e))
+    async def load(self, ctx, *, module: str) -> None:
+        """
+        the module to load, classes with commands.Cog
+        :param ctx: context of the invocation
+        :param module: proper name of the cog class
+        :return: None
+        """
+        if module.lower() == 'help':
+            await ctx.channel.send(f'The {module} can only be reloaded.')
         else:
-            await ctx.channel.send(f'Loaded module {module}')
+            try:
+                self.bot.add_cog(cog_mapping[module])
+            except Exception as e:
+                log.error('{}: {}'.format(type(e).__name__, e))
+            else:
+                await ctx.channel.send(f'Loaded module {module}')
 
     @has_role(roles()['admin'])
     @commands.command(brief='unload a module, module names are listed in the help menu overview',
                       usage='{}unload'.format(config['prefix']))
-    async def unload(self, ctx, *, module: str):
-        try:
-            self.bot.remove_cog(module)
-        except Exception as e:
-            log.error('{}: {}'.format(type(e).__name__, e))
+    async def unload(self, ctx, *, module: str) -> None:
+        """
+        the module to unload, classes with commands.Cog
+        :param ctx: context of the invocation
+        :param module: proper name of the cog class
+        :return: None
+        """
+        if module.lower() == 'help':
+            await ctx.channel.send(f'The module {module} cannot be unloaded')
         else:
-            await ctx.channel.send(f'Unloaded module {module}')
+            try:
+                self.bot.remove_cog(module)
+            except Exception as e:
+                log.error('{}: {}'.format(type(e).__name__, e))
+            else:
+                await ctx.channel.send(f'Unloaded module {module}')
 
     @has_role(roles()['admin'])
     @commands.command(name='reload', brief='reload a module, module names are listed in the help menu overview',
                       usage='{}reload'.format(config['prefix']))
-    async def _reload(self, ctx, *, module: str):
+    async def _reload(self, ctx, *, module: str) -> None:
+        """
+        the module to reload, classes with commands.Cog
+        :param ctx: context of the invocation
+        :param module: proper name of the cog class
+        :return: None
+        """
         try:
             self.bot.remove_cog(module)
             self.bot.add_cog(cog_mapping[module])
