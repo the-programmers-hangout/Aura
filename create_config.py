a234a6d8627e768cb7bf616e718acdcf99cd021b
e-xe <undested@gmail.com>
2020-07-10 17:04:59 +0545

    Added pipenv and black/flake8 with configuration

--- create_config.py
+++ create_config.py
@@ -1,6 +1,6 @@
 import os
 import shutil
 
-if __name__ == '__main__':
-    if not os.path.exists('config.yaml'):
-        shutil.copy('config.yaml.example', 'config.yaml')
+if __name__ == "__main__":
+    if not os.path.exists("config.yaml"):
+        shutil.copy("config.yaml.example", "config.yaml")
